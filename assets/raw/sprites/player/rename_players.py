5db2776800dc3217263a437e7f871c63dd8ba234
Harrison Lambeth <harrison@nau.edu>
2015-04-28 21:13:58 -0700

    Updated spritesheet Added some new player animations

--- assets/raw/sprites/player/rename_players.py
+++ assets/raw/sprites/player/rename_players.py
@@ -16,7 +16,11 @@ replacements = {
 	15: "Player_Air_Med_",
 	16: "Player_Air_Slow_",
 	17: "Player_Air_Peak_",
+	21: "Player_Attack_",
+	23: "Player_Teleport_Out_",
 }
+keys = replacements.keys()
+keys.sort()
 
 i = 1
 prev_r = 0
@@ -26,7 +30,7 @@ while True:
 	if isfile(fname):
 		replacement = ""
 		index = 0
-		for r in replacements.keys():
+		for r in keys:
 			if r >= i:
 				replacement = replacements[r]
 				index = i - prev_r
@@ -35,6 +39,7 @@ while True:
 				break
 		new_fname = replacement + ("{0:0=" + str(digits) + "d}").format(index) + originalExt
 		rename(fname, new_fname)
+		print fname + "\t" + new_fname
 		i += 1
 	else:
 		break
