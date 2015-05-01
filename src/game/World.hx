e758b77a045be363f370d658c1a97b80d6719237
Harrison Lambeth <harrison@nau.edu>
2015-04-30 19:05:21 -0700

    Added ability to filter collisions by layer

--- src/game/World.hx
+++ src/game/World.hx
@@ -221,7 +221,7 @@ class World extends Sprite {
 	// Pass a collider of something you want to test the collision of (the player's ship for example).
 	// optionally pass in collisionInfo to retrieve an array of collisions that occured with some (admittedly not super reliable) data about them.
 	// This function returns True if there was a collision and False if not.
-	public function checkCollision(collider:Collider, ?collisionInfo:Array<CollisionInformation>):Bool {
+	public function checkCollision(collider:Collider, ?collisionInfo:Array<CollisionInformation>, ?layerFilter:Array<String> ):Bool {
 		
 		if (collisionInfo == null)
 			collisionInfo = new Array<CollisionInformation>();
@@ -231,7 +231,7 @@ class World extends Sprite {
 		for (c in colliders) {
 			ci = new CollisionInformation();
 			if (collider != c && collider.getOwner() != c.getOwner()
-				&& collisionMatrix.canCollide(collider, c)) {
+				&& collisionMatrix.canCollide(collider, c, layerFilter)) {
 					if(collider.isClipping(c, ci)) {
 						var collide = false;
 						
